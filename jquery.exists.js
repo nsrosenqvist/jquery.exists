8dd3495ff80bb48e463164eb0400f15f9c32a7e0
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2018-09-05 16:23:36 +0200

    No longer requires callback

--- jquery.exists.js
+++ jquery.exists.js
@@ -2,10 +2,19 @@
   $.fn.exists = function(callback) {
     var args = [].slice.call(arguments, 1);
 
-    if (this.length) {
-      callback.call(this, args);
-    }
+    if (callback) {
+      if (this.length) {
+        callback.call(this, args);
+      }
 
-    return this;
+      return this;
+    }
+    else {
+      if (this.length) {
+        return true;
+      }
+
+      return false;
+    }
   };
 }(jQuery));
